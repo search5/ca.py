de4d94a5542ad42450a6f4b0d46fe932b8f086a7
ji-ho lee <jhlee@webiznet.com>
2016-12-28 18:41:26 +0900

    setup.py의 gitthub.com 경로 수정

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'CA.py',
     packages = ['ca_py'], # this must be the same as the name above
-    version = '1.2',
+    version = '1.3',
     description = 'CA.pl Python Fork',
     author = 'Lee Ji-Ho',
     author_email = 'search5@gmail.com',
-    url = 'https://github.com/search5/ca_py.py', # use the URL to the github repo
-    download_url = 'https://github.com/search5/ca_py.py/tarball/1.2', # I'll explain this in a second
+    url = 'https://github.com/search5/ca.py', # use the URL to the github repo
+    download_url = 'https://github.com/search5/ca.py/tarball/1.3', # I'll explain this in a second
     keywords = ['openssl', 'ca', 'cert'], # arbitrary keywords
     classifiers = [
         "Development Status :: 4 - Beta",
