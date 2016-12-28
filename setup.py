810e6c12d871490d443613720fe200dc0eaf81f7
ji-ho lee <jhlee@webiznet.com>
2016-12-28 12:08:21 +0900

    pypi upload ready

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+    name = 'CA.py',
+    packages = ['ca'], # this must be the same as the name above
+    version = '1.0',
+    description = 'CA.pl Python Fork',
+    author = 'Lee Ji-Ho',
+    author_email = 'search5@gmail.com',
+    url = 'https://github.com/search5/ca.py', # use the URL to the github repo
+    download_url = 'https://github.com/search5/ca.py/tarball/1.0', # I'll explain this in a second
+    keywords = ['openssl', 'ca', 'cert'], # arbitrary keywords
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows :: Windows 7",
+        "Operating System :: Microsoft :: Windows :: Windows XP",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: System :: Networking",
+        "Topic :: Security :: Cryptography"
+    ]
+)
